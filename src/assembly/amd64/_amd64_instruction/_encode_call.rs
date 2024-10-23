54dddc29e61879c492f634da0f8b96227a837387
Cayden Lund <cayden.lund@utah.edu>
2024-10-23 14:41:50 -0600

    Encode `call <func>`

--- src/assembly/amd64/_amd64_instruction/_encode_call.rs
+++ src/assembly/amd64/_amd64_instruction/_encode_call.rs
@@ -1,12 +1,18 @@
 use crate::assembly::amd64::AMD64Operand::*;
-use crate::assembly::amd64::{AMD64Instruction, AMD64Operand, Function, ModRM, Rex, Sib};
-use crate::assembly::Instruction;
+use crate::assembly::amd64::{AMD64Instruction, Function};
 
 impl AMD64Instruction {
-    pub(crate) fn encode_call(
-        self: &AMD64Instruction,
-        tgt: &Function,
-    ) -> Vec<u8> {
-        todo!()
+    pub(crate) fn encode_call(self: &AMD64Instruction, tgt: &Function) -> Vec<u8> {
+        let tgt_name = match tgt {
+            Function::GetChar => std::ffi::CString::new("getchar").unwrap(),
+            Function::PutChar => std::ffi::CString::new("putchar").unwrap(),
+        };
+        let tgt_addr = unsafe { libc::dlsym(libc::RTLD_DEFAULT, tgt_name.as_ptr()) as usize };
+
+        vec![0x48, 0xB8]
+            .into_iter()
+            .chain(self.unwrap(Self::encode_imm(tgt_addr as isize, 64)))
+            .chain(vec![0xFF, 0xD0])
+            .collect()
     }
 }
