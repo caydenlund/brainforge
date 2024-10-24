02c90db7792a4c379622f1ffaa9c3c339836b197
Cayden Lund <cayden.lund@utah.edu>
2024-10-24 01:56:57 -0600

    Encode `je <tgt>`

--- src/assembly/amd64/_amd64_instruction/_encode_je.rs
+++ src/assembly/amd64/_amd64_instruction/_encode_je.rs
@@ -3,6 +3,9 @@ use crate::BFResult;
 
 impl AMD64Instruction {
     pub(crate) fn encode_je(self: &AMD64Instruction, tgt: isize) -> BFResult<Vec<u8>> {
-        todo!()
+        Ok(vec![0x0F, 0x84]
+            .into_iter()
+            .chain(self.encode_imm(tgt, 32)?)
+            .collect())
     }
 }
